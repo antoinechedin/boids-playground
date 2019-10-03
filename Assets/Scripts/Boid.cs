bf281743bda7dcfb4b61dfe4331aeaf7bcc963fe
antoinechedin <antoine.chedin@gmail.com>
2019-10-03 18:00:10 +0200

    Add unity project and boids

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    public float maxSpeed, minSpeed;
+    public float maxForce;
+
+    public Transform target;
+    public Box box;
+
+    private void Start()
+    {
+        velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * (maxSpeed + minSpeed) / 2f ;
+    }
+
+    private void Update()
+    {
+        Vector3 acceleration = Vector3.zero;
+
+        acceleration += box.alignementCoef * Alignement();
+        acceleration += box.cohesionCoef * Cohesion();
+        acceleration += box.separationCoef * Separation();
+
+        velocity += acceleration;
+        float speed = Mathf.Clamp(velocity.magnitude, minSpeed, maxSpeed);
+        transform.position += velocity.normalized * speed;
+
+        transform.LookAt(transform.position + velocity);
+
+        // Teleport boid
+        if (transform.position.x > box.size / 2f)
+            transform.position += new Vector3(-box.size, 0, 0);
+        if (transform.position.x < -box.size / 2f)
+            transform.position += new Vector3(box.size, 0, 0);
+        if (transform.position.y > box.size / 2f)
+            transform.position += new Vector3(0, -box.size, 0);
+        if (transform.position.y < -box.size / 2f)
+            transform.position += new Vector3(0, box.size, 0);
+        if (transform.position.z > box.size / 2f)
+            transform.position += new Vector3(0, 0, -box.size);
+        if (transform.position.z < -box.size / 2f)
+            transform.position += new Vector3(0, 0, box.size);
+    }
+
+    public Vector3 Alignement()
+    {
+        Vector3 avgVelocity = Vector3.zero;
+        int numNeighbor = 0;
+        foreach (Boid boid in box.boids)
+        {
+            if (boid != this)
+            {
+                float t = (boid.transform.position - transform.position).magnitude;
+                if (t < box.separationDistance)
+                {
+                    avgVelocity += boid.velocity;
+                    numNeighbor++;
+                }
+            }
+        }
+        if (numNeighbor > 0)
+        {
+            avgVelocity /= numNeighbor;
+
+            return Steer(transform.position + avgVelocity);
+        }
+        return Vector3.zero;
+    }
+
+    public Vector3 Cohesion()
+    {
+        Vector3 centerOfNeighbors = Vector3.zero;
+        int numNeighbor = 0;
+        foreach (Boid boid in box.boids)
+        {
+            if (boid != this)
+            {
+                float t = (boid.transform.position - transform.position).magnitude;
+                if (t < box.neighborDistance)
+                {
+                    centerOfNeighbors += boid.transform.position;
+                    numNeighbor++;
+                }
+            }
+        }
+        if (numNeighbor > 0)
+        {
+            centerOfNeighbors /= numNeighbor;
+            return Steer(centerOfNeighbors);
+        }
+        return Vector3.zero;
+    }
+
+    public Vector3 Separation()
+    {
+
+        Vector3 centerOfNeighbors = Vector3.zero;
+        int numNeighbor = 0;
+        foreach (Boid boid in box.boids)
+        {
+            if (boid != this)
+            {
+                float t = (boid.transform.position - transform.position).magnitude;
+                if (t < box.separationDistance)
+                {
+                    centerOfNeighbors += boid.transform.position;
+                    numNeighbor++;
+                }
+            }
+        }
+        if (numNeighbor > 0)
+        {
+            centerOfNeighbors /= numNeighbor;
+
+
+            return Steer(transform.position - (centerOfNeighbors - transform.position));
+        }
+        return Vector3.zero;
+    }
+
+    public Vector3 Steer(Vector3 target)
+    {
+        Vector3 desired = (target - transform.position);
+        desired = desired.normalized * maxSpeed - velocity;
+        return Vector3.ClampMagnitude(desired, maxForce);
+    }
+}
---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public float size;
+
+    public GameObject boidPrefab;
+    public int numBoid = 10;
+
+    [Range(0, 3)]
+    public float alignementCoef = 1, cohesionCoef = 1, separationCoef = 1;
+
+    public float separationDistance = 3f;
+    public float neighborDistance = 10f;
+
+    [HideInInspector]
+    public List<Boid> boids = new List<Boid>();
+
+    private void Start()
+    {
+        for (int i = 0; i < numBoid; i++)
+        {
+            Boid boid = Instantiate(boidPrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<Boid>();
+            boid.box = this;
+            boid.transform.position = new Vector3(Random.Range(-size / 2f, size / 2f), Random.Range(-size / 2f, size / 2f), Random.Range(-size / 2f, size / 2f));
+            boids.Add(boid);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, new Vector3(size, size, size));
+    }
+}
